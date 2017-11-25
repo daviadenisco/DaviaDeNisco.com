977c25c9b660a0bd04b72ec3361c9cbfc780bc20
Davia DeNisco <davia622@gmail.com>
2017-11-25 18:05:15 -0500

    make snake food appear

--- snake/script.js
+++ snake/script.js
@@ -1,22 +1,39 @@
+//===================================
+// UTILITIES
+//===================================
 
-// create variable for row div class so we can increase that variable and name for each div row so it properly labels each row we want to add
-// add columns to each row div
-// append div row elements and add columns
-
+// VARIABLES FOR ROWS AND COLUMNS
 let rows = 50;
 let cols = 50;
+
+// GET ANY ELEMENT ON THE GRID
+function getElement(rowNum, colNum) {
+  let rowSelector = '.r' + rowNum.toString();
+  // class .col within each row will have only one col with the random num in the class
+  return document.querySelector(rowSelector + ' .c' + colNum.toString())
+}
+
+// GENERATE RANDOM NUM
+function randoNumGen(amt) {
+  return Math.floor(Math.random() * amt);
+}
+
+//====================================
+// SNAKE STUFF
+//====================================
+
+// CREATE GRID
+// append div row elements and append columns to div rows
 function createGrid(numRows, numCols) {
   let grid = document.getElementById("grid");
-
   // i is the row
-  for(let i = 0; i < numRows; i++) {
+  for (let i = 0; i < numRows; i++) {
     // creates the div for the new row
     let newRow = document.createElement("div");
     // sets newRow className to row r"i"
     newRow.className = "row " + "r" + i.toString()
-
     // j is the column
-    for(let j = 0; j < numCols; j++) {
+    for (let j = 0; j < numCols; j++) {
       // creates the div for the new row
       let newCol = document.createElement("div");
       // sets newCol classname to col c"j"
@@ -30,31 +47,29 @@ function createGrid(numRows, numCols) {
 }
 createGrid(rows, cols);
 
-function startingPoint(max) {
-  return Math.floor(Math.random() * max);
-}
-// the getRandom function runs only upon a page reload
-// currentRow and currentCol
-let currentRow = startingPoint(rows);
-let currentCol = startingPoint(cols);
+// CREATE VARIABLES
+// track the current coordinates of the snake head
+// increment and decrement to "move" the snake
+let currentRow = randoNumGen(rows);
+let currentCol = randoNumGen(cols);
+// current row and current col for element retrieval
 let cC;
-let cR;
 
-function drawSnake() {
-  let rowSelector = '.r' + currentRow.toString();
-  cR = document.querySelector(rowSelector);
-  // class .col within each row will have only one col with the random num in the class
-  cC = document.querySelector(rowSelector + ' .c' + currentCol.toString())
+// ADD SNAKE CLASS to current grid space
+function addSnakeClass() {
   // just .class will not work, and must include space before the className so it doesn't interfere with the other class names
+  cC = getElement(currentRow, currentCol)
   cC.className += " snake";
 }
-drawSnake();
+addSnakeClass();
 
+// TOGGLE SNAKE CLASS
+// adds or removes snake class to "move" snake head around the board
 function toggleSnakeClass(currentColumn) {
   cC.classList.toggle("snake");
-  // cR.classList.toggle("snake");
 }
 
+// ADD EVENT LISTENERS TO ARROW KEYS
 // Add event listeners to the arrow keys. These keys will move snakey. When someone hits the “left” arrow key, you want the current location of snakey to decrease by one column:
 document.onkeydown = checkKey;
 
@@ -63,31 +78,34 @@ function checkKey(e) {
   if (e.keyCode == '38') {
     // up arrow, decrease by one row
     currentRow -= 1;
-  }
-  else if (e.keyCode == '40') {
+  } else if (e.keyCode == '40') {
     // down arrow, increase by one row
     currentRow += 1;
-  }
-  else if (e.keyCode == '37') {
-   // left arrow, decrease by one col
-   currentCol -= 1;
-  }
-  else if (e.keyCode == '39') {
-   // right arrow, increase by one col
-   currentCol += 1;
+  } else if (e.keyCode == '37') {
+    // left arrow, decrease by one col
+    currentCol -= 1;
+  } else if (e.keyCode == '39') {
+    // right arrow, increase by one col
+    currentCol += 1;
   }
   toggleSnakeClass(cC);
-  drawSnake();
+  addSnakeClass();
 }
 
-// function addSpider(currentRow, currentCol) {
-//   let spiderRow = '.sp' + currentRow.toString();
-//   let rSr = document.querySelector(rowSelector);
-//   let rSc = document.querySelector(rowSelector + '.sp' + currentCol.toString())
-//   // if grid !includes #spider && currentRow && currentCol !include .snake
-//   // add addSpider
-//   // else
-//   // addSpider(currentRow, currentCol);
-//   rSc.background = "spider.jpg";
-// }
-// addSpider(currentRow, currentCol);
+//=========================================
+// BUGS STUFF
+//=========================================
+
+// 1. PICK RANDOM COL FROM GRID
+// 2. ADD BUGS CLASS TO THAT RANDOM COL
+// 3. USE setTimeout TO REMOVE THE BUGS CLASS FROM THE RANDOM COL IF IT IS STILL THERE
+
+function triggerBug() {
+  let bugRow = randoNumGen(rows);
+  let bugCol = randoNumGen(cols);
+
+  let bugEl = getElement(bugRow, bugCol);
+  // adds or removes bug class to make bug appear in diff places around the board
+  bugEl.classList.toggle("bug");
+}
+triggerBug();
