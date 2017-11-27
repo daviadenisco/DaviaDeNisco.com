2e16c9f6a06acc8a46c32be48c325eeb55c44560
Davia DeNisco <davia622@gmail.com>
2017-11-27 14:51:18 -0500

    display score q exit git status quit .exit Q

--- snake/script.js
+++ snake/script.js
@@ -1,26 +1,14 @@
-//===================================
+//========================================
 // UTILITIES
-//===================================
+//========================================
 
 // VARIABLES FOR ROWS AND COLUMNS
 let rows = 50;
 let cols = 50;
 
-// GET ANY ELEMENT ON THE GRID
-function getElement(rowNum, colNum) {
-  let rowSelector = '.r' + rowNum.toString();
-  // class .col within each row will have only one col with the random num in the class
-  return document.querySelector(rowSelector + ' .c' + colNum.toString())
-}
-
-// GENERATE RANDOM NUM
-function randoNumGen(amt) {
-  return Math.floor(Math.random() * amt);
-}
-
-//====================================
-// SNAKE STUFF
-//====================================
+// VARIABLE TO TALLY SCORE
+let scoreDisplay = document.getElementById("score");
+let score = 0;
 
 // CREATE GRID
 // append div row elements and append columns to div rows
@@ -47,6 +35,22 @@ function createGrid(numRows, numCols) {
 }
 createGrid(rows, cols);
 
+// GET ANY ELEMENT ON THE GRID
+function getElement(rowNum, colNum) {
+  let rowSelector = '.r' + rowNum.toString();
+  // class .col within each row will have only one col with the random num in the class
+  return document.querySelector(rowSelector + ' .c' + colNum.toString())
+}
+
+// GENERATE RANDOM NUM
+function randoNumGen(amt) {
+  return Math.floor(Math.random() * amt);
+}
+
+//========================================
+// SNAKE STUFF
+//========================================
+
 // CREATE VARIABLES
 // track the current coordinates of the snake head
 // increment and decrement to "move" the snake
@@ -54,6 +58,8 @@ let currentRow = randoNumGen(rows);
 let currentCol = randoNumGen(cols);
 // current row and current col for element retrieval
 let cC;
+// snake location / length array
+let snakeArr =[];
 
 // ADD SNAKE CLASS to current grid space
 function addSnakeClass() {
@@ -75,30 +81,58 @@ document.onkeydown = checkKey;
 
 function checkKey(e) {
   e = e || window.event;
+  let currentElement = getElement(currentRow, currentCol);
   if (e.keyCode == '38') {
     // up arrow, decrease by one row
     currentRow -= 1;
+    // check whether currentElement has the class of bug
+    if (currentElement.classList[2] === "bug") {
+      currentElement.classList.remove("bug");
+      score += 1;
+      triggerBug();
+      // make snake grow by 1
+    }
   } else if (e.keyCode == '40') {
     // down arrow, increase by one row
     currentRow += 1;
+    if (currentElement.classList[2] === "bug") {
+      currentElement.classList.remove("bug");
+      score += 1;
+      triggerBug();
+      // make snake grow by 1
+    }
   } else if (e.keyCode == '37') {
     // left arrow, decrease by one col
     currentCol -= 1;
+    if (currentElement.classList[2] === "bug") {
+      currentElement.classList.remove("bug");
+      score += 1;
+      triggerBug();
+      // make snake grow by 1
+    }
   } else if (e.keyCode == '39') {
     // right arrow, increase by one col
     currentCol += 1;
+    if (currentElement.classList[2] === "bug") {
+      currentElement.classList.remove("bug");
+      score += 1;
+      triggerBug();
+      // make snake grow by 1
+    }
   }
+  scoreDisplay.innerHTML = score;
   toggleSnakeClass(cC);
   addSnakeClass();
 }
 
-//=========================================
+//========================================
 // BUGS STUFF
-//=========================================
+//========================================
 
 // 1. PICK RANDOM COL FROM GRID
 // 2. ADD BUGS CLASS TO THAT RANDOM COL
 // 3. USE setTimeout TO REMOVE THE BUGS CLASS FROM THE RANDOM COL IF IT IS STILL THERE
+
 
 function triggerBug() {
   let bugRow = randoNumGen(rows);
@@ -107,5 +141,9 @@ function triggerBug() {
   let bugEl = getElement(bugRow, bugCol);
   // adds or removes bug class to make bug appear in diff places around the board
   bugEl.classList.toggle("bug");
+  // sets timeout for bug to appear after 3 seconds, instead of the other way around
+  setTimeout(function(){
+    bugEl.classList.toggle("bug");
+  }, 5000);
 }
 triggerBug();
