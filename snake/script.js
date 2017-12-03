21c0742d61167201b00e5d2333d169c9e53371f0
Davia DeNisco <davia622@gmail.com>
2017-12-03 14:25:16 -0500

    make snake grow

--- snake/script.js
+++ snake/script.js
@@ -1,6 +1,16 @@
+// NEXT STEPS
+// feed snakeArr currentElement
+// make snake grow
+// game over screen with option to try again
+// game over if hit "wall"
+// game over if hit "snake"
+
 //========================================
 // UTILITIES
 //========================================
+
+//VARIABLE FOR GRID
+let grid = document.getElementById("grid");
 
 // VARIABLES FOR ROWS AND COLUMNS
 let rows = 50;
@@ -10,10 +20,35 @@ let cols = 50;
 let scoreDisplay = document.getElementById("score");
 let score = 0;
 
+// CREATE VARIABLES
+// track the current coordinates of the snake head
+// increment and decrement to "move" the snake
+let currentRow = randoNumGen(rows);
+let currentCol = randoNumGen(cols);
+
+// GET CURRENT LOCATION ELEMENT
+let currentElement = getElement(currentRow, currentCol);
+
+// CURRENT ROW AND CURRENT COL FOR ELEMENT RETRIEVAL
+let cC;
+
+// SNAKE LOCATION / LENGTH ARRAY
+let snakeArr = [];
+
+// VARIABLE TO TARGET POTENTIAL NEW POSITION
+let potentialNewPosition;
+// VARIABLE TO TRY AGAIN / START A NEW GAME
+let resetButton = document.querySelector("#newGame");
+
+// VARIABLE TO TRACK WHETHER BUGS EATEN
+let bugEaten = false;
+
+// VARIABLE FOR POTENTIAL NEW POSITION CLASS LIST
+// let potNewPosCL = potentialNewPosition.classList;
+
 // CREATE GRID
 // append div row elements and append columns to div rows
 function createGrid(numRows, numCols) {
-  let grid = document.getElementById("grid");
   // i is the row
   for (let i = 0; i < numRows; i++) {
     // creates the div for the new row
@@ -51,28 +86,22 @@ function randoNumGen(amt) {
 // SNAKE STUFF
 //========================================
 
-// CREATE VARIABLES
-// track the current coordinates of the snake head
-// increment and decrement to "move" the snake
-let currentRow = randoNumGen(rows);
-let currentCol = randoNumGen(cols);
-// current row and current col for element retrieval
-let cC;
-// snake location / length array
-let snakeArr =[];
-
 // ADD SNAKE CLASS to current grid space
 function addSnakeClass() {
   // just .class will not work, and must include space before the className so it doesn't interfere with the other class names
   cC = getElement(currentRow, currentCol)
-  cC.className += " snake";
+  cC.classList.add("snake");
 }
 addSnakeClass();
+snakeArr.push(cC);
 
-// TOGGLE SNAKE CLASS
-// adds or removes snake class to "move" snake head around the board
-function toggleSnakeClass(currentColumn) {
-  cC.classList.toggle("snake");
+// MOVE AND / OR GROW SNAKE
+function moveSnake(nextElement, currentElement) {
+  if (bugEaten === false) {
+    snakeArr.pop().classList.remove("snake");
+  }
+  snakeArr.unshift(nextElement);
+  bugEaten = false;
 }
 
 // ADD EVENT LISTENERS TO ARROW KEYS
@@ -81,48 +110,42 @@ document.onkeydown = checkKey;
 
 function checkKey(e) {
   e = e || window.event;
-  let currentElement = getElement(currentRow, currentCol);
   if (e.keyCode == '38') {
+    // gets the element of the next potential position of the snake
+    potentialNewPosition = getElement(currentRow - 1, currentCol);
     // up arrow, decrease by one row
     currentRow -= 1;
-    // check whether currentElement has the class of bug
-    if (currentElement.classList[2] === "bug") {
-      currentElement.classList.remove("bug");
-      score += 1;
-      triggerBug();
-      // make snake grow by 1
-    }
+
   } else if (e.keyCode == '40') {
+    // gets the element of the next potential position of the snake
+    potentialNewPosition = getElement(currentRow + 1, currentCol);
     // down arrow, increase by one row
     currentRow += 1;
-    if (currentElement.classList[2] === "bug") {
-      currentElement.classList.remove("bug");
-      score += 1;
-      triggerBug();
-      // make snake grow by 1
-    }
+
   } else if (e.keyCode == '37') {
+    // gets the element of the next potential position of the snake
+    potentialNewPosition = getElement(currentRow, currentCol - 1);
     // left arrow, decrease by one col
     currentCol -= 1;
-    if (currentElement.classList[2] === "bug") {
-      currentElement.classList.remove("bug");
-      score += 1;
-      triggerBug();
-      // make snake grow by 1
-    }
+
   } else if (e.keyCode == '39') {
+    // gets the element of the next potential position of the snake
+    potentialNewPosition = getElement(currentRow, currentCol + 1);
     // right arrow, increase by one col
     currentCol += 1;
-    if (currentElement.classList[2] === "bug") {
-      currentElement.classList.remove("bug");
-      score += 1;
-      triggerBug();
-      // make snake grow by 1
-    }
+
   }
-  scoreDisplay.innerHTML = score;
-  toggleSnakeClass(cC);
+  // adds snake class to cC
   addSnakeClass();
+  if (potentialNewPosition.classList[2] === "bug") {
+    potentialNewPosition.classList.remove("bug");
+    score += 1;
+    triggerBug();
+    bugEaten = true;
+  }
+  // move snake and/or grow snake by 1
+  moveSnake(potentialNewPosition, currentElement);
+  scoreDisplay.innerHTML = score * 100;
 }
 
 //========================================
@@ -133,17 +156,39 @@ function checkKey(e) {
 // 2. ADD BUGS CLASS TO THAT RANDOM COL
 // 3. USE setTimeout TO REMOVE THE BUGS CLASS FROM THE RANDOM COL IF IT IS STILL THERE
 
-
 function triggerBug() {
   let bugRow = randoNumGen(rows);
   let bugCol = randoNumGen(cols);
-
   let bugEl = getElement(bugRow, bugCol);
+  console.log("bugEl", bugEl);
   // adds or removes bug class to make bug appear in diff places around the board
-  bugEl.classList.toggle("bug");
+  // HOW CAN I EFFICIENTLY GOOGLE HOW TO DO THIS?
+  // THAT'S NESTED HOW TO DO BY THE WAY
+  if (bugEl.classList[2] !== "snake") {
+    bugEl.classList.add("bug");
+    setTimeout(function() {
+      bugEl.classList.toggle("bug");
+    }, 10000);
+  } else {
+    bugEl = getElement(bugRow, bugCol);
+  }
   // sets timeout for bug to appear after 3 seconds, instead of the other way around
-  setTimeout(function(){
-    bugEl.classList.toggle("bug");
-  }, 10000);
 }
 triggerBug();
+
+//========================================
+// NEW GAME STUFF
+//========================================
+resetButton.addEventListener("click", function(){
+  // remove current grid
+  grid.innerHTML = "";
+  // generate new grid
+  createGrid(rows, cols);
+  // reset snake class on random element
+  currentRow = randoNumGen(rows);
+  currentCol = randoNumGen(cols);
+  // add snake class
+  addSnakeClass();
+  // reset bug class on random element
+  triggerBug();
+});
